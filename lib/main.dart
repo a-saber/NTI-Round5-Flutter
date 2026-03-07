4ee4ebd2f702213b34f91077cc28b8d8b0ab8ae1
a-saber <ahs629862@gmail.com>
2026-03-07 11:36:35 +0200

    2026/03/07 sat messenger screen

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,105 @@ class MyApp extends StatelessWidget{
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       // design 1st screen
-      home: Scaffold(
+      home: ProfileTask(),
+    );
+  }
+}
+
+class ProfileTask extends StatelessWidget {
+  const ProfileTask({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
+
+
+class MessageScreen extends StatelessWidget {
+  const MessageScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Hi'),
+        actions: [
+          Icon(Icons.check)
+        ],
+      ),
+      body: Column(
+        children:
+        [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children:
+            [
+              DefaultCircle(),
+              DefaultCircle(),
+              DefaultCircle(),
+            ],
+          ),
+          SizedBox(height: 100,),
+          Container(
+            width: double.infinity,
+            height: 50,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              border: Border.all(
+                color: Colors.grey,
+              )
+            ),
+            child: Row(
+              children:
+              [
+                CircleAvatar(
+                  radius: 25,
+                  backgroundColor: Colors.blue,
+                ),
+                SizedBox(width: 15,),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: 
+                  [
+                    Text('Hi'),
+                    Text('Ahmed'),
+                  ],
+                ),
+                Spacer(),
+                Icon(Icons.check)
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+class DefaultCircle extends StatelessWidget {
+  const DefaultCircle({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 100,
+      width: 100,
+      decoration: BoxDecoration(
+          color: Colors.red,
+          shape: BoxShape.circle
+      ),
+      child: Center(child: Text('A')),
+    );
+  }
+}
+
+
+class Test01 extends StatelessWidget{
+  const Test01({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
         appBar: AppBar(
           centerTitle: true,
           backgroundColor: Colors.white,
@@ -25,75 +123,74 @@ class MyApp extends StatelessWidget{
           ],
         ),
         body: Container(
-          color: Colors.grey,
-          width: double.infinity,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              SizedBox(height: 50,),
-              Container(
-                height: 300,
-                width: 100,
-                // color: Colors.red,
-                decoration: BoxDecoration(
+            color: Colors.grey,
+            width: double.infinity,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                SizedBox(height: 50,),
+                Container(
+                  height: 300,
+                  width: 100,
                   // color: Colors.red,
-                  borderRadius: BorderRadius.circular(20),
-                  shape: BoxShape.rectangle,
-                  gradient: LinearGradient(
-                    begin: Alignment.topCenter,
-                    end: Alignment.bottomCenter,
-                    colors: [
-                      Colors.blue,
-                      Colors.red,
-                      Colors.black,
-                    ]
+                  decoration: BoxDecoration(
+                    // color: Colors.red,
+                      borderRadius: BorderRadius.circular(20),
+                      shape: BoxShape.rectangle,
+                      gradient: LinearGradient(
+                          begin: Alignment.topCenter,
+                          end: Alignment.bottomCenter,
+                          colors: [
+                            Colors.blue,
+                            Colors.red,
+                            Colors.black,
+                          ]
+                      ),
+                      boxShadow: [
+                        BoxShadow(
+                            color: Colors.black.withValues(alpha: 1),
+                            offset: Offset(10, 0),
+                            spreadRadius: 3,
+                            blurRadius: 10
+                        ),
+                        BoxShadow(
+                            color: Colors.red,
+                            offset: Offset(-10, 0),
+                            spreadRadius: 3,
+                            blurRadius: 10
+                        ),
+                      ]
                   ),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black.withValues(alpha: 1),
-                      offset: Offset(10, 0),
-                      spreadRadius: 3,
-                      blurRadius: 10
-                    ),
-                    BoxShadow(
-                      color: Colors.red,
-                      offset: Offset(-10, 0),
-                      spreadRadius: 3,
-                      blurRadius: 10
-                    ),
-                  ]
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('data'),
-                    Text('data'),
-                  ],
-                ),
-              ),
-              // Spacer(),
-              SizedBox(height: 50,),
-              Expanded(
-                flex: 2,
-                child: Container(
-                  color: Colors.green,
                   child: Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Text('data'),
                       Text('data'),
                     ],
                   ),
                 ),
-              ),
+                // Spacer(),
+                SizedBox(height: 50,),
+                Expanded(
+                  flex: 2,
+                  child: Container(
+                    color: Colors.green,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text('data'),
+                        Text('data'),
+                      ],
+                    ),
+                  ),
+                ),
 
 
-            ],
-          )
+              ],
+            )
         )
-      ),
     );
   }
 }
